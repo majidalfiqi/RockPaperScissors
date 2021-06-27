60dcc7aa72f18575d16f7c1bf2d88eaa653e5dbf
Majed Alfiqi <majidalfiqi@gmail.com>
2021-06-27 05:26:32 +0100

    added favicon.ico, finished result.html and styling of game.html and result.html, added query selector, initializations, event listeners and basic logic to game.js

--- js/game.js
+++ js/game.js
@@ -1,86 +1,116 @@
-function computerPlay() {
+// query selectors
+plyrRock = document.querySelector(".plyr-rock");
+plyrPaper = document.querySelector(".plyr-paper");
+plyrScissors = document.querySelector(".plyr-scissors");
+
+comRock = document.querySelector(".com-rock");
+comPaper = document.querySelector(".com-paper");
+comScissors = document.querySelector(".com-scissors");
+
+prompt = document.querySelector(".prompt");
+
+roundResult = document.querySelector(".round-result");
+result = document.querySelector(".result");
+
+roundDetails = document.querySelector(".round-details");
+winChoice = document.querySelector(".win-choice");
+loseChoice = document.querySelector(".lose-choice");
+
+roundNumber = document.querySelector("#round-number");
+comScore = document.querySelector("#com-score");
+plyrScore = document.querySelector("#player-score");
+
+// initializations
+let round = 1;
+roundNumber.textContent = round;
+
+let com = 0;
+comScore.textContent = com;
+let plyr = 0;
+plyrScore.textContent = plyr;
+
+let comChoice = "";
+let plyrChoice = "";
+
+let resultText = "";
+
+let win = "";
+let lose = "";
+
+// event listeners
+plyrRock.addEventListener("click", () => {
+  plyrChoice = "rock";
+  comChoice = comPlay();
+  game(plyrChoice, comChoice);
+});
+plyrPaper.addEventListener("click", () => {
+  plyrChoice = "paper";
+  comChoice = comPlay();
+  game(plyrChoice, comChoice);
+});
+plyrScissors.addEventListener("click", () => {
+  plyrChoice = "scissors";
+  comChoice = comPlay();
+  game(plyrChoice, comChoice);
+});
+
+// logic
+function comPlay() {
   let sel = ["rock", "paper", "scissors"];
   let i = Math.floor(Math.random() * 3);
   return sel[i];
 }
 
-function processInput(input) {
-  try {
-    input = input.toLowerCase();
-  } catch {
-    return null;
-  }
-  switch (input) {
-    case "r":
-    case "rock":
-      input = "rock";
-      break;
-    case "p":
-    case "paper":
-      input = "paper";
-      break;
-    case "s":
-    case "scissors":
-      input = "scissors";
-      break;
-    default:
-      return null;
-  }
-  return input;
-}
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
+function playRound(plyrChoice, comChoice) {
+  if (plyrChoice === comChoice) {
     return "tie";
   }
 
-  let win;
-  switch (computerSelection) {
+  let result;
+  switch (comChoice) {
     case "rock":
-      win = playerSelection === "paper";
+      plyrChoice === "paper" ? (result = "WON") : (result = "LOST");
       break;
     case "paper":
-      win = playerSelection === "scissors";
+      plyrChoice === "scissors" ? (result = "WON") : (result = "LOST");
       break;
     case "scissors":
-      win = playerSelection === "rock";
+      plyrChoice === "rock" ? (result = "WON") : (result = "LOST");
       break;
     default:
-      return "Error!";
-  }
-  win
-    ? console.log(`You win the round! ${playerSelection} beats ${computerSelection}`)
-    : console.log(`You lose the round! ${computerSelection} beats ${playerSelection}`);
-  win
-    ? (msg.innerText = `You win the round! ${playerSelection} beats ${computerSelection}`)
-    : (msg.innerText = `You lose the round! ${computerSelection} beats ${playerSelection}`);
-  return win;
-}
-
-function game() {
-  let com = 0;
-  let plyr = 0;
-
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = null;
-    while (playerSelection === null) {
-      playerSelection = window.prompt("Enter your choice. rock, paper, or scissors?", "");
-      playerSelection = processInput(playerSelection);
-    }
-    const computerSelection = computerPlay();
-    let round = playRound(playerSelection, computerSelection);
-    if (round === "tie") {
-      console.log("It's a tie. Try again!");
-      msg.innerText = "It's a tie. Try again!";
-      i--;
-    }
-    round ? plyr++ : com++;
+      return "error";
   }
 
-  plyr > com ? console.log("You win the game!!!") : console.log("You lose the game :(");
-  plyr > com ? (msg.innerText = "You win the game!!!") : (msg.innerText = "You lose the game :(");
+  return result;
 }
 
-msg = document.getElementById("message");
+function game(plyrChoice, comChoice) {
+  resultText = playRound(plyrChoice, comChoice);
+  switch (resultText) {
+    case "tie":
+      roundResult.textContent = "It's a tie!";
+      result.textContent = " X";
+      result.classList.add("tie");
+      roundResult.appendChild(result);
+      prompt.classList.toggle("hidden");
+      roundResult.classList.toggle("hidden");
+      break;
+    case "WON":
+      plyr++;
+      plyrScore.textContent = plyr;
+      break;
+    case "LOST":
+      com++;
+      comScore.textContent = com;
+      break;
+  }
+  if (round < 5) {
+    round++;
+    roundNumber.textContent = round;
+  } else {
+    // move to result.html with the result
+  }
+}
+
 // game();
 //window.location.replace("http://www.w3schools.com");
